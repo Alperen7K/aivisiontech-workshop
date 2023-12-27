4b805e113a8b5c7cb9cf93a2f3c8f8cf8e3a36cb
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2023-12-27 23:09:13 +0300

    faz 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import logo from "./aivisiontechWhite.svg";
 import termo from "./termoHUMAN.png";
 import { motion } from "framer-motion";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
-import deneme from "./03.json";
-import deneme1 from "./lottieflow-background-01-F48127-easey.json";
+import ai4sports from "./ai4sportsWhite.svg";
+import { HumanModal } from "./component";
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -13,72 +12,118 @@ const container = {
     opacity: 1,
     scale: 1,
     transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
+      delayChildren: 1.5,
+      staggerChildren: 0.4,
     },
   },
 };
 
 const item = {
-  hidden: { y: 20, opacity: 0 },
+  hidden: { x: -50, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+const navbarContainer = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const navbarItem = {
+  hidden: { y: -25, opacity: 0 },
   visible: {
     y: 0,
     opacity: 1,
   },
 };
 
+// #D6E1ED beyazımsı
+// #5DB19B yeşil
+// #4E9CE8 mavimsi
+// #F77F93 PEMBE
+
 export const App = () => {
   let labelClass =
-    "font-bold px-2 text-[#EEEEEE] hover:border-b-[3px] border-solid hover:border-[#4ECCA3] box-border";
+    "font-bold bg-[#232931] text-[20px] px-2 text-[#EEEEEE] hover:border-b-[3px] border-solid hover:border-[#4ECCA3] box-border";
 
   return (
     <div className="h-[100vh] w-[100vw]  flex flex-col items-center">
-      <div className="w-full h-[12vh] flex items-start justify-center bg-[#232931] border-solid border-b-4 border-[#4ECCA3] shadow-xl">
+      {/* Navbar */}
+      <div className=" w-full h-[14vh] flex items-start justify-center bg-[#232931] border-solid border-b-4 border-[#4ECCA3] shadow-xl">
         <div className="w-[85vw] h-full flex items-center justify-between">
-          <img className="h-[85px]" src={logo} />
+          <img className="dropShadow h-[100px]" src={logo} />
           <div className="flex items-center">
-            <label className={labelClass}>ÜRÜN</label>
-            <label className={labelClass}>HAKKIMIZDA</label>
-            <label className={labelClass}>İLETİŞİM</label>
-            <label className={labelClass}>HABERLER</label>
+            <motion.ul
+              className="flex items-center"
+              variants={navbarContainer}
+              initial="hidden"
+              animate="visible"
+            >
+              <motion.li variants={navbarItem}>
+                <label className={labelClass}>ÜRÜN</label>
+              </motion.li>
+              <motion.li variants={navbarItem}>
+                <label className={labelClass}>HAKKIMIZDA</label>
+              </motion.li>
+              <motion.li variants={navbarItem}>
+                <label className={labelClass}>İLETİŞİM</label>
+              </motion.li>
+              <motion.li variants={navbarItem}>
+                <label className={labelClass}>HABERLER</label>
+              </motion.li>
+            </motion.ul>
           </div>
         </div>
       </div>
+
       <div className="bg-[#393E46] h-full w-full">
         <div className="bg h-[88vh] w-full flex items-center justify-evenly ">
-          <div className="">
+          {/* <Player
+            autoplay
+            loop
+            src={deneme}
+            className="dropShadow"
+            style={{ height: "400px" }}
+          /> */}
+          <div className=" ml-16">
+            <div className="  flex items-center justify-center ">
+              <img
+                className="bg-[#232931] rounded-[30px]  dropShadow w-[10vw] pr-2 border-solid  border-[#4ECCA3] border-4"
+                src={ai4sports}
+              />
+            </div>
             <motion.ul
-              className="  grid grid-cols-2 gap-[10px]"
+              className="gap-[10px]"
               variants={container}
               initial="hidden"
               animate="visible"
             >
-              {[0, 1, 2, 3].map((index) => (
-                <motion.li
-                  key={index}
-                  className="w-[10vw] h-[10vw] bg-[#4ECCA3] text-[#eee]"
-                  variants={item}
-                >
-                  {index}
-                </motion.li>
-              ))}
+              {/* <motion.li variants={item}> */}
+              {/* </motion.li> */}
+
+              <motion.li className=" text-[#eee] w-fit" variants={item}>
+                <h1 className="dropShadow text-[60px] border-solid border-[#4ECCA3] border-b-4 border-r-4 rounded-br-xl pb-0 pr-3 mt-4 w-fit font-bold ">
+                  Fark Edilmeyen
+                </h1>
+              </motion.li>
+              <motion.li className=" text-[#eee]" variants={item}>
+                <h1 className="dropShadow text-[60px] border-solid border-[#4ECCA3] border-b-4 border-r-4 rounded-br-xl pb-0 pr-3 mt-4 w-fit font-bold ">
+                  Sakatlıklara SON!
+                </h1>
+              </motion.li>
             </motion.ul>
           </div>
-          <img className="dropShadow h-[30vw] " src={termo} />
+          <HumanModal />
         </div>
       </div>
-      {/* <Player
-        autoplay
-        loop
-        src={deneme}
-        style={{ height: "300px", width: "300px" }}
-      />
-      <Player
-        autoplay
-        loop
-        src={deneme1}
-        style={{ height: "300px", width: "300px" }}
-      /> */}
     </div>
   );
 };
---
+++ src/component/HumanModal.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, PresentationControls, Stage } from "@react-three/drei";
+
+export const HumanModal = () => {
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setY((prevValue) => prevValue + 0.005);
+    }, 10);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <Canvas
+      className="dropShadow"
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 60 }}
+      style={{ width: "40vw" }}
+    >
+      <PresentationControls
+        speed={4}
+        global
+        zoom={0.5}
+        rotation={[0, y, 0]}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage environment={"sunset"}>
+          <Modal scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+  );
+};
+
+function Modal(props) {
+  const { scene } = useGLTF("/human.glb");
+  return <primitive object={scene} scale={0.01} {...props} />;
+}
